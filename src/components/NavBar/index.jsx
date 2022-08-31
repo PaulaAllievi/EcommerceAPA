e4afae044f4533b248057ee48bf0eaf0270b0c0b
anapaula <allievianapaula@gmail.com>
2022-08-30 22:18:07 -0300

    Sumando NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const NavBar = () => {
+    return (
+        
+        <nav className="nav">
+            <ul>
+                <li><a>Inicio</a></li>
+                <li><a>Productos</a></li>
+                <li><a>Mayoristas</a></li>
+                <li><a>Nosotros</a></li>
+            </ul> 
+        </nav>
+    
+   
+    )
+}
+
+export default NavBar
+
