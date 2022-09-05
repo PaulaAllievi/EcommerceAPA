82940b8ed0e91e3914035c3ace5b4546df69e3c3
anapaula <allievianapaula@gmail.com>
2022-09-05 16:42:05 -0300

    contador

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,42 @@
+import React, {useState,useEffect} from "react";
+import './styles.css';
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    
+    const [count, setCount] = useState(parseInt(initial));
+    
+    const handleAdd = () => {
+        if (count < stock) {
+            setCount(count+1);
+        }
+        else {
+            alert("No hay stock suficiente");
+        }     
+    }
+
+    const handleDecrement = () => {
+            setCount(count-1);                
+    }
+
+    useEffect(() => {
+        setCount(parseInt(initial));
+    }, [initial])
+
+    return (
+        <>
+        <div className="contador">
+        <div className="contadorHijo">
+            <button className="button" disabled={count <= 1} onClick={handleDecrement}>-</button>
+            <h2>{count}</h2>
+            <button className="button" onClick={handleAdd}>+</button>
+        </div>
+        <div>
+            <button className="buttonAgregar" disabled={stock <=0} onClick={() => onAdd(count)}>Agregar al carrito</button>
+        </div>
+        </div>
+        </> 
+
+      );    
+}
+
+export default ItemCount;
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import ItemCount from '../../components/ItemCount'
 import './styles.css'
 
 export const ItemListContainer = ({greeting}) => {
+  const agregarAlCarrito = (cantidad) => {
+    console.log(`se agrego ${cantidad}`) 
+  }
   return (
     <div>
        <h1>{greeting}</h1> 
+       <ItemCount initial={1} stock={5} onAdd={agregarAlCarrito} />
     </div>
   )
-}
+
+
+};
 
 export default ItemListContainer;
 
